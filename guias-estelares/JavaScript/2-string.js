44290c9f164dd0f94ddab09c4e235121a2bcccc0
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-04-27 08:11:42 -0300

    starting tipos de dados

---
+++ guias-estelares/JavaScript/2-string.js
@@ -0,0 +1,11 @@
+console.log("Thiago Carlos")
+console.log('Limeira da Silva')
+console.log(`com crases eu posso 
+
+quebrar linha
+
+`)
+
+console.log("um 'exemplo'com asplas duplas")
+console.log('um "exemplo" com aspas simples')
+console.log(`um "exemplo" com 'crases'`)
---
+++ guias-estelares/JavaScript/scripts.js
@@ -0,0 +1,9 @@
+/*
+Boolean - Booleano
+    * é um tipo de dado que só tem 2 valores
+    true // verdadeiro
+    false - falso
+*/
+
+console.log(true)
+console.log(false)
