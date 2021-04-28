649028a3f066b7115eac44eced5a4639c39d09b8
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-04-28 07:58:12 -0300

    Starting variáveis

---
+++ guias-estelares/JavaScript/6-object.js
@@ -0,0 +1,16 @@
+/*
+* Object
+    * Objeto
+    * Dado estrutural
+    * Proriedades / Atributos
+    * Funcionalidades / Métodos
+{propriedade: "valor"}
+*/
+
+console.log({
+    name: "Thiago",
+    idade: 33,
+    andar: function(){
+        console.log('andar')
+    }
+})
