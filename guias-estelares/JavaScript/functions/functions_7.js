cee485fa03d9ac93a93abd3cb6b07f9c36e54c6f
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-04-30 08:10:43 -0300

    finished functions

---
+++ guias-estelares/JavaScript/functions/functions_7.js
@@ -0,0 +1,13 @@
+// arrow function
+
+const sayMyName = () => {
+    console.log('Thiago')
+}
+
+sayMyName()
+
+const sayMyNickName = (nickname) => {
+    console.log(nickname)
+}
+
+sayMyNickName('Carlos')
