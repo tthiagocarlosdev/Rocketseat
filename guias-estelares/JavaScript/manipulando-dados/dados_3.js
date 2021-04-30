7063597a57ebb4cb0c0d7d58de04092c5d4346c1
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-04-30 15:03:55 -0300

    finished manipulação de dados

---
+++ guias-estelares/JavaScript/manipulando-dados/dados_3.js
@@ -0,0 +1,9 @@
+/*
+    Manipulando Strings e Números
+    Transformar String em Número e Número em String
+*/
+
+let string = "123"
+let number = 321
+console.log(Number(string))
+console.log(String(number))
---
+++ guias-estelares/JavaScript/manipulando-dados/dados_9.js
@@ -0,0 +1,10 @@
+/*
+    Manipulando Arrays
+    Criar Array com construtor
+*/
+let myArray1 = ['a', 'b', 'c']
+console.log(myArray1)
+let myArray2 = new Array('d', 'e', 'f')
+console.log(myArray2)
+let myArray3 = new Array(10)
+console.log(myArray3)
