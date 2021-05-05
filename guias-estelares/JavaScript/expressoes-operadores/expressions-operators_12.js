1d9099b6fb320864e78159b84bfe11f5062917a4
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-05-05 08:00:07 -0300

    finished expressions and operators

---
+++ guias-estelares/JavaScript/expressoes-operadores/expressions-operators_12.js
@@ -0,0 +1,9 @@
+// Operador de String (String operator)
+// comparison (comparação)
+console.log ('a' == 'b')
+
+// concatenation (concatenação)
+// Retorna a união de duas Strings
+let alpha = 'alpha'
+alpha += 'bet'
+console.log(alpha + 345)
---
+++ guias-estelares/JavaScript/expressoes-operadores/expressions-operators_9.js
@@ -0,0 +1,25 @@
+// Operadores de atribuição (Assignment)
+let x
+// assignment
+x = 1
+// addition assignment
+//x = x + 2
+x += 2
+console.log(x)
+// subtraction assignment
+// x = x -1
+x -= 1
+console.log(x)
+// multiplication assignment
+// x = x * 2
+x *= 2
+console.log(x)
+// division assignment
+// x = x / 2
+x /= 2
+console.log(x)
+// remainder, exponetiation
+x %= 2
+console.log(x)
+x **= 2
+console.log(x)
