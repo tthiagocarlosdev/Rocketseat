9f4467c50a5e5252897a41cc9c99c94e42463403
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-05-06 09:21:56 -0300

    started and finished conditions and control flow

---
+++ guias-estelares/JavaScript/condicionais controle de fluxo /condicionais_01.js
@@ -0,0 +1,9 @@
+console.log("Controle de fluxo de aplicação")
+// Control flow
+console.log('a')
+console.log('b')
+
+//se estiver sujo
+console.log('lavar o copo')
+//se não estiver sujo
+console.log('servir café')
