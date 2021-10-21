f7c90c4d583304a0fa1c34718964937ad386410a
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-10-21 12:49:56 -0300

    exerciciosJavaScriptRocketseat finished ex004_dadosArray.js

--- guias-estelares/JavaScript/exerciciosJavaScriptRocketseat/ex004_dadosArray.js
+++ guias-estelares/JavaScript/exerciciosJavaScriptRocketseat/ex004_dadosArray.js
@@ -11,6 +11,9 @@ function menu(){
     console.log('Menu')
     console.log('[1] nº Categorias')
     console.log('[2] nº de Livros p/ Categoria')
+    console.log('[3] Total de autores')
+    console.log('[4] Livros por autor')
+    console.log('[5] Finalizar programa!')
     return input.question('')
 }
 
@@ -19,6 +22,32 @@ function simOuNao(){
     console.log('[1] Sim')
     console.log('[2] Não')
     return input.question('')
+}
+
+function listaDeAutores(){
+    let autores = []
+    for(let categoria of booksByCategory){
+        for(let livros of categoria.books){
+            if(autores.includes(livros.author) == false){
+                autores.push(livros.author)
+            }
+        }
+    }
+    return autores
+}
+
+
+
+function livrosDoAutor(autor){
+    let livrosDoAutor = []
+    for(let categoria of booksByCategory){
+        for(let livros of categoria.books){
+            if(livros.author.toUpperCase() === autor.toUpperCase()){
+                livrosDoAutor.push(livros.title)
+            }
+        }
+    }
+    return livrosDoAutor
 }
 
 //variáveis
@@ -59,38 +88,38 @@ const booksByCategory = [
     },
 ]
 const quantidadeCategorias = booksByCategory.length
-let quantlivrosTotal = 0
-let quantAutoresTotal = 0
 
-for(i = 0; i < quantidadeCategorias; i++){
-    quantlivrosTotal += booksByCategory[i].books.length
-}
-console.log(`Nº Livros Totais: ${quantlivrosTotal}`)
-
-for(i = 0; i < quantidadeCategorias; i++){
-    quantAutoresTotal += booksByCategory[i].books[i].author.length
-}
-console.log(`Nº Autores Totais: ${quantAutoresTotal}`)
-
-/*
 do {
     header()
     escolha = Number(menu())
     console.clear()
     header()
-    if(escolha == 1){
-        console.log(`Quantidade de Categorias: ${quantidadeCategorias}`)
-    } else if (escolha == 2){
-        for(i = 0; i < quantidadeCategorias; i++) {
-            let quantidadeLivrosPorCategoria = booksByCategory[i].books.length
-            console.log(`Quantidade de livros na ${i+1}º Categoria: ${quantidadeLivrosPorCategoria}`)
-        }
+    switch(escolha){
+        case 1:
+            console.log(`Quantidade de Categorias: ${quantidadeCategorias}`)
+            break
+        case 2:
+            for(let categoria of booksByCategory){
+                console.log(`Total de livros da categoria ${categoria.category}: ${categoria.books.length}`)            
+            }        
+            break
+        case 3:
+            console.log(`Total de autores: ${listaDeAutores().length}`)
+            break
+        case 4:
+            console.log(listaDeAutores())
+            let escolhaAutor = input.question('Digite o nome do autor: ')
+            console.clear()
+            header()
+            console.log(`Livros do autor ${escolhaAutor}: ${livrosDoAutor(escolhaAutor).join(', ')}`)
+            break
     }
+
     console.log('---------------------------')
     continuar = simOuNao()
     console.clear()
 } while (continuar < 2)
-*/
+
 header()
 console.log('Programa Finalizado!')
 console.log('===========================')
