17be310fcae4e78e0e1a74b40948304af25fbcc6
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-10-27 23:16:36 -0300

    Launchbase finished desafio1-4_operacoesBancarias.js

--- LAUNCHBASE/aulas/desafio1-4_operacoesBancarias.js
+++ LAUNCHBASE/aulas/desafio1-4_operacoesBancarias.js
@@ -17,38 +17,69 @@ function createTransaction(type, value){
 
 function getHigherTransactionByType(typeTransaction){
   let higherValue = 0
+  let objectHigherValue = 0
   
   if(typeTransaction == 'credit'){
     for(let i = 0; i < user.transactions.length; i++){
       if(user.transactions[i].type == 'credit' && higherValue < user.transactions[i].value){
-        higherValue = user.transactions[i]
+        higherValue = user.transactions[i].value
+        objectHigherValue = user.transactions[i]
       }
     }
 
   } else if(typeTransaction == 'debit'){
     for(let i = 0; i < user.transactions.length; i++){
       if(user.transactions[i].type == 'debit' && higherValue < user.transactions[i].value){
-        higherValue = user.transactions[i]
+        higherValue = user.transactions[i].value
+        objectHigherValue = user.transactions[i]
       }
     }
   }
   
-  return higherValue
+  return objectHigherValue
   
 }
 
-createTransaction('credit', 100)
-createTransaction('debit', 10)
-createTransaction('debit', 50)
-createTransaction('credit', 150)
+function getAverageTransactionValue(){
+  let sumTransactions = 0
+  let averageTransactions = 0
+
+  for(let i = 0; i < user.transactions.length; i++){
+    sumTransactions +=user.transactions[i].value
+  }
+
+  averageTransactions = sumTransactions / user.transactions.length
+
+  return averageTransactions
+
+}
+
+function getTransactionsCount(){
+  let objectCreditDebit = {
+    credit: 0,
+    debit: 0,
+  }
+
+  for(let i = 0; i < user.transactions.length; i++){
+    user.transactions[i].type == 'credit' ? objectCreditDebit.credit++ : objectCreditDebit.debit++
+  }
+
+  return objectCreditDebit
+
+}
+
+
 createTransaction('credit', 50)
+createTransaction('credit', 120)
+createTransaction('debit', 80)
 createTransaction('debit', 30)
 
-
-//console.log(user)
+console.log(user)
 console.table(user.transactions)
 console.log(`Balance: ${user.balance.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`)
-console.log(`Transactions: ${user.transactions.length}`)
 console.log("Higher Transactions:")
 console.log(getHigherTransactionByType('credit'))
 console.log(getHigherTransactionByType('debit'))
+console.log(`Average Transactions: ${getAverageTransactionValue()}`)
+console.log(`Total Transactions: ${user.transactions.length}`)
+console.log(getTransactionsCount())
