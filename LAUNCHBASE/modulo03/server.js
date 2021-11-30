46c838a74e12ffdca53acea1035de67bb1c2c425
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-11-30 07:29:10 -0300

    Launchbase from Alinhando elementos com CSS Grid to Atualizando página sobre

--- LAUNCHBASE/modulo03/server.js
+++ LAUNCHBASE/modulo03/server.js
@@ -9,11 +9,23 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get("/", function(req, res){
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars.githubusercontent.com/u/76978748?v=4",
+    name:"Thiago Carlos",
+    role: "Estudante da Rocketseat",
+    description: 'Aluno de desenvolvimento front-end, focado em aprender programação web na escola <a href="https://www.rocketseat.com.br/" target="_blank">Rocketseat',
+    links:[
+      { name: "GitHub", url: "https://github.com/tthiagocarlosdev" },
+      { name: "Youtube", url: "https://www.youtube.com/channel/UCZN-uQtc4UDQt_tLu-I7Wpw" },
+      { name: "Instagram", url: "https://www.instagram.com/tthiagocarlos.dev/" }
+    ]
+  }
+  return res.render("about", {about})
 })
 
 server.get("/portifolio", function(req, res){
